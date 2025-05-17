de7d0dd3d49178b4ac903c884aa23410a25f3e1d
nuts01110526 <nuts01110526@gmail.com>
2025-05-17 16:59:12 +0900

    next.jsチュートリアル

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'customers',
+};export default function Page() {
     return <p>Customers Page</p>;
   }
